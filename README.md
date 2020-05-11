89949d9f6610f88415d007e8b3931477cb809750
Sen <46561474+obsoletenerd@users.noreply.github.com>
2020-05-11 15:54:05 +1000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# arcade-macro-keyboard
-A macro keyboard made of arcade machine buttons. Acts like a normal USB keyboard, but each arcade button runs multi-key macros/shortcuts.
+# Arcade Macro Keyboard
+
+A macro keyboard made of arcade buttons. Acts like a normal USB keyboard, but each arcade button runs multi-key macros/shortcuts.
+
+Example use:
+
+I use it as a Stream Deck using OBS for Twitch/YouTube streaming. I set the "keyCodes" in the code to numbers 1-7 (ASCII 49-55), and the modifiers to CTRL, ALT, and SHIFT. Pressing arcade button 1 then sends the macro CTRL+ALT+SHIFT+1 to Windows, which I have mapped in OBS to switch to Scene 1. Etc etc.
+
+It will work everywhere in Windows, and just acts like a standard USB keyboard inserting the shortcuts as though you yourself pressed it on your primary keyboard.
+
+Number of buttons doesn't matter, you can do more or less, as long as you have enough inputs and modify the code appropriately to add/remove them.
+
+## Wiring Diagram
+
+![Arcade Macro Keyboard Diagram](https://github.com/obsoletenerd/arcade-macro-keyboard/blob/master/WiringDiagram.png)
+
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+/*
+Arcade Macro Keyboard
+
+Install this on an Arduino Nano.
+Wire up some Arcade buttons (or any buttons), and pushing one will run a multi-key keyboard shortcut.
+Acts like a standard USB Keyboard in Windows.
+
+@ObsoleteNerd
+*/
+
+#include <Arduino.h>
+#include <Keyboard.h>
+
+// Modifiers for key combos (https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardmodifiers)
+char ctrlKey = KEY_LEFT_CTRL;
+char altKey = KEY_LEFT_ALT;
+char shiftKey = KEY_LEFT_SHIFT;
+
+#define buttonAmount 7 // How many buttons do you have total
+
+int buttonPins[] = { 2, 3, 4, 5, 6, 7, 8 }; // Pins the buttons are connected to
+int keyCodes[] = { 49, 50, 51, 52, 53, 54, 55 }; // ASCII codes for each of the above buttons (http://www.asciitable.com/) In this case it's the numbers 1-7
+int buttonState[buttonAmount];
+
+void setup() {
+  for (int i = 0; i < buttonAmount; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP); // Make all the buttons inputs and turn on the pullup resistor
+  }
+  Keyboard.begin(); // Initialise keyboard control
+}
+
+void loop() {
+  for (int i = 0; i < buttonAmount; i++) {
+    buttonState[i] = digitalRead(buttonPins[i]);
+    if (buttonState[i] == LOW){ // Check if a button has been pressed, and if so, perform the combo + the initialised button's associated ASCII code
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press(keyCodes[i]);
+      delay(100); // Give it a (micro)second to register
+      Keyboard.releaseAll(); // Release all keys
+    }
+  }
+}
